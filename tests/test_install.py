32a6fa357c35f679d2ea6e639e8ad34f7de9493f
Thomas Kluyver <takowl@gmail.com>
2015-03-31 15:48:40 -0700

    testpath can now assert on pathlib paths

--- tests/test_install.py
+++ tests/test_install.py
@@ -26,17 +26,17 @@ class InstallTests(TestCase):
 
     def test_install_module(self):
         Installer(samples_dir / 'module1-pkg.ini').install()
-        assert_isfile(str(self.tmpdir / 'site-packages' / 'module1.py'))
-        assert_isdir(str(self.tmpdir / 'site-packages' / 'module1-0.1.egg-info'))
+        assert_isfile(self.tmpdir / 'site-packages' / 'module1.py')
+        assert_isdir(self.tmpdir / 'site-packages' / 'module1-0.1.egg-info')
 
     def test_install_package(self):
         Installer(samples_dir / 'package1-pkg.ini').install()
-        assert_isdir(str(self.tmpdir / 'site-packages' / 'package1'))
-        assert_isdir(str(self.tmpdir / 'site-packages' / 'package1-0.1.egg-info'))
-        assert_isfile(str(self.tmpdir / 'scripts' / 'pkg_script'))
+        assert_isdir(self.tmpdir / 'site-packages' / 'package1')
+        assert_isdir(self.tmpdir / 'site-packages' / 'package1-0.1.egg-info')
+        assert_isfile(self.tmpdir / 'scripts' / 'pkg_script')
 
     def test_symlink_package(self):
         Installer(samples_dir / 'package1-pkg.ini', symlink=True).install()
-        assert_islink(str(self.tmpdir / 'site-packages' / 'package1'),
+        assert_islink(self.tmpdir / 'site-packages' / 'package1',
                       to=str(samples_dir / 'package1'))
-        assert_isfile(str(self.tmpdir / 'scripts' / 'pkg_script'))
+        assert_isfile(self.tmpdir / 'scripts' / 'pkg_script')
--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -16,14 +16,14 @@ def clear_samples_dist():
 def test_wheel_module():
     clear_samples_dist()
     WheelBuilder(samples_dir / 'module1-pkg.ini').build()
-    assert_isfile(str(samples_dir / 'dist/module1-0.1-py2.py3-none-any.whl'))
+    assert_isfile(samples_dir / 'dist/module1-0.1-py2.py3-none-any.whl')
 
 def test_wheel_package():
     clear_samples_dist()
     WheelBuilder(samples_dir / 'package1-pkg.ini').build()
-    assert_isfile(str(samples_dir / 'dist/package1-0.1-py2.py3-none-any.whl'))
+    assert_isfile(samples_dir / 'dist/package1-0.1-py2.py3-none-any.whl')
 
 def test_dist_name():
     clear_samples_dist()
     WheelBuilder(samples_dir / 'altdistname.ini').build()
-    assert_isfile(str(samples_dir / 'dist/packagedist1-0.1-py2.py3-none-any.whl'))
+    assert_isfile(samples_dir / 'dist/packagedist1-0.1-py2.py3-none-any.whl')
