95c60fd4901808103358809461fced7b5f98baf5
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-04-14 14:08:44 -0700

    mata -> meta

--- flit/__init__.py
+++ flit/__init__.py
@@ -69,7 +69,7 @@ def main(argv=None):
             sys.exit(e.args[0])
     elif args.subcmd == 'register':
         from .upload import register
-        mod, meta = common.matadata_and_module_from_ini_file(args.ini_file)
+        mod, meta = common.metadata_and_module_from_ini_file(args.ini_file)
         register(meta)
     elif args.subcmd == 'init':
         from .init import TerminalIniter
