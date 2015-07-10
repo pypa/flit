7d5b69133fcc74184de9b402552ff33447a7bdcb
Caleb Hattingh <caleb.hattingh@gmail.com>
2015-07-10 12:17:18 +1000

    Fix wrong names.

--- tests/samples/package1/__init__.py
+++ tests/samples/package1/__init__.py
@@ -1,13 +1,13 @@
 """A sample package"""
 
 __version__ = '0.1'
-__flit_module__ = 'package_caleb'
+__flit_module__ = 'package1'
 __author__ = 'Sir Robin'
 __author_email__ = 'robin@camelot.uk'
-__home_page__ = 'http://github.com/sirrobin/package_caleb'
-__scripts__ = [
-        'package-caleb:main'
-        ]
+__home_page__ = 'http://github.com/sirrobin/package1'
+__scripts__ = {
+        'pkg-script': 'package1:main'
+        }
 
 def main():
     print("package1 main")
