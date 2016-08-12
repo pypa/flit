a8f0e42f52ee3519f1fd9ee6c3bfcb09302ed63e
Thomas Kluyver <thomas@kluyver.me.uk>
2016-08-12 15:28:08 +0100

    Add test for loading info from .toml file

--- tests/test_inifile.py
+++ tests/test_inifile.py
@@ -17,3 +17,8 @@ def test_missing_entrypoints():
 def test_flatten_entrypoints():
     r = flatten_entrypoints({'a': {'b': {'c':'d'}, 'e': {'f': {'g':'h'}}, 'i':'j'}})
     assert r == {'a': {'i': 'j'}, 'a.b': {'c': 'd'}, 'a.e.f': {'g': 'h'}}
+
+def test_load_toml():
+    inf = read_pkg_ini(samples_dir / 'module1-pkg.toml')
+    assert inf['module'] == 'module1'
+    assert inf['metadata']['home_page'] == 'http://github.com/sirrobin/module1'
