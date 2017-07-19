87dc647b949b45afcfe2f8a5dafb5b6657f3bb7c
Thomas Kluyver <thomas@kluyver.me.uk>
2017-07-19 17:47:14 +0100

    Add a test for prompting for an option

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,3 +1,4 @@
+import builtins
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from unittest.mock import patch
@@ -11,3 +12,28 @@ def test_store_defaults():
             d = {'author': 'Test'}
             init.store_defaults(d)
             assert init.get_defaults() == d
+
+def fake_input(entries):
+    it = iter(entries)
+    def inner(prompt):
+        try:
+            return next(it)
+        except StopIteration:
+            raise EOFError
+
+    return inner
+
+def faking_input(entries):
+    return patch.object(builtins, 'input', fake_input(entries))
+
+def test_prompt_options():
+    ti = init.TerminalIniter()
+    with faking_input(['4', '1']):
+        res = ti.prompt_options('Pick one', [('A', 'Apple'), ('B', 'Banana')])
+    assert res == 'A'
+
+    # Test with a default
+    with faking_input(['']):
+        res = ti.prompt_options('Pick one', [('A', 'Apple'), ('B', 'Banana')],
+                                default='B')
+    assert res == 'B'
