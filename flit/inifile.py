f3528031562d6067d059ac087b794cef7a453f1e
Thomas Kluyver <thomas@kluyver.me.uk>
2017-07-25 17:42:57 +0100

    Add test for 'did you mean' error message

--- flit/inifile.py
+++ flit/inifile.py
@@ -35,6 +35,7 @@ metadata_allowed_fields = {
     'requires-python',
     'dist-name',
     'entry-points-file',
+    'description-file',
 } | metadata_list_fields
 
 metadata_required_fields = {
--- tests/test_inifile.py
+++ tests/test_inifile.py
@@ -14,6 +14,12 @@ def test_missing_entrypoints():
     with pytest.raises(FileNotFoundError):
         read_pkg_ini(samples_dir / 'entrypoints_missing.ini')
 
+def test_misspelled_key():
+    with pytest.raises(ConfigError) as e_info:
+        read_pkg_ini(samples_dir / 'misspelled-key.ini')
+
+    assert 'description-file' in str(e_info)
+
 def test_description_file():
     info = read_pkg_ini(samples_dir / 'package1-pkg.ini')
     assert info['metadata']['description'] == \
