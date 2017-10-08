487a6a824151a65f23a495886dbee4cd7b0dca20
Thomas Kluyver <thomas@kluyver.me.uk>
2017-10-08 18:45:44 +0100

    Update build API for latest draft of PEP 517

--- flit/buildapi.py
+++ flit/buildapi.py
@@ -11,12 +11,12 @@ log = logging.getLogger(__name__)
 # PEP 517 specifies that the CWD will always be the source tree
 pyproj_toml = Path('pyproject.toml')
 
-def get_build_requires(config_settings):
+def get_requires_for_build_wheel(config_settings):
     """Returns a list of requirements for building, as strings"""
     info = read_pkg_ini(pyproj_toml)
     return info['metadata']['requires-dist']
 
-def get_wheel_metadata(metadata_directory, config_settings):
+def prepare_metadata_for_build_wheel(metadata_directory, config_settings):
     """Creates {metadata_directory}/foo-1.2.dist-info"""
     ini_info = read_pkg_ini(pyproj_toml)
     module = Module(ini_info['module'], Path.cwd())
@@ -38,10 +38,10 @@ def get_wheel_metadata(metadata_directory, config_settings):
         with (dist_info / 'entry_points.txt').open('w') as f:
             write_entry_points(ini_info['entrypoints'], f)
 
+    return dist_info.name
+
 def build_wheel(wheel_directory, config_settings, metadata_directory=None):
     """Builds a wheel, places it in wheel_directory"""
-    make_wheel_in(pyproj_toml, Path(wheel_directory))
+    _, path = make_wheel_in(pyproj_toml, Path(wheel_directory))
+    return path.name
 
-
-# Editable installs - API still under discussion.
-# def install_editable(prefix, config_settings, metadata_directory=None):
