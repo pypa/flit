bf7d35864cfe992b32007bbcdb9b38e739d038f3
Atsushi Odagiri <aodagx@gmail.com>
2018-01-17 00:52:33 +0900

    added test case for pkgutil style namespace package.

--- tests/test_install.py
+++ tests/test_install.py
@@ -84,6 +84,12 @@ class InstallTests(TestCase):
         assert_isdir(self.tmpdir / 'site-packages' / 'ns1')
         assert_isdir(self.tmpdir / 'site-packages' / 'ns1.pkg-0.1.dist-info')
 
+    def test_install_ns_package(self):
+        Installer(samples_dir / 'ns2-pkg.toml').install_directly()
+        assert_isdir(self.tmpdir / 'site-packages' / 'ns2')
+        assert_isfile(self.tmpdir / 'site-packages' / 'ns2' / '__init__.py')
+        assert_isdir(self.tmpdir / 'site-packages' / 'ns2.pkg-0.1.dist-info')
+
     def test_symlink_package(self):
         if os.name == 'nt':
             raise SkipTest("symlink")
