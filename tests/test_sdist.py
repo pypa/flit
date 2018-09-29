201bb600799ff29b933b0e05f24e14748666ed7f
Thomas Kluyver <thomas@kluyver.me.uk>
2018-09-29 09:14:13 +0200

    Fix generating setup.py with extras & environment markers
    Closes gh-202

--- tests/test_sdist.py
+++ tests/test_sdist.py
@@ -76,18 +76,39 @@ def test_get_files_list_hg():
             'bar', pjoin('subdir', 'qux')
         }
 
-def test_make_setup_py():
-    builder = sdist.SdistBuilder(samples_dir / 'package1-pkg.ini')
-    setup = builder.make_setup_py()
+def get_setup_assigns(setup):
+    """Parse setup.py, execute assignments, return the namespace"""
     setup_ast = ast.parse(setup)
     # Select only assignment statements
     setup_ast.body = [n for n in setup_ast.body if isinstance(n, ast.Assign)]
     ns = {}
     exec(compile(setup_ast, filename="setup.py", mode="exec"), ns)
+    return ns
+
+def test_make_setup_py():
+    builder = sdist.SdistBuilder(samples_dir / 'package1-pkg.ini')
+    ns = get_setup_assigns(builder.make_setup_py())
     assert ns['packages'] == ['package1', 'package1.subpkg', 'package1.subpkg2']
     assert 'install_requires' not in ns
     assert ns['entry_points'] == \
            {'console_scripts': ['pkg_script = package1:main']}
+
+def test_make_setup_py_reqs():
+    builder = sdist.SdistBuilder(samples_dir / 'extras.toml')
+    ns = get_setup_assigns(builder.make_setup_py())
+    assert ns['install_requires'] == ['toml']
+    assert ns['extras_require'] == {'test': ['pytest'], 'custom': ['requests']}
+
+def test_make_setup_py_reqs_envmark():
+    builder = sdist.SdistBuilder(samples_dir / 'requires-envmark.toml')
+    ns = get_setup_assigns(builder.make_setup_py())
+    assert ns['install_requires'] == ['requests']
+    assert ns['extras_require'] == {":python_version == '2.7'": ['pathlib2']}
+
+def test_make_setup_py_reqs_extra_envmark():
+    builder = sdist.SdistBuilder(samples_dir / 'requires-extra-envmark.toml')
+    ns = get_setup_assigns(builder.make_setup_py())
+    assert ns['extras_require'] == {'test:python_version == "2.7"': ['pathlib2']}
 
 def test_clean_tarinfo():
     with tarfile.open(mode='w', fileobj=BytesIO()) as tf:
