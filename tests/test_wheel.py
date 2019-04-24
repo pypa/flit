9c67580ed5ccc36195827ecbe9a15cc9e4a8e77f
Brian Okken <1568356+okken@users.noreply.github.com>
2019-04-23 21:38:28 -0700

    tests for src versions

--- tests/test_wheel.py
+++ tests/test_wheel.py
@@ -36,6 +36,19 @@ def test_wheel_package():
     wheel_main(samples_dir / 'package1-pkg.ini')
     assert_isfile(samples_dir / 'dist/package1-0.1-py2.py3-none-any.whl')
 
+def test_wheel_src_module():
+    clear_samples_dist()
+    wheel_main(samples_dir / 'module3-pkg.ini')
+    assert_isfile(samples_dir / 'dist/module3-0.1-py2.py3-none-any.whl')
+
+def test_wheel_src_package():
+    try:
+        shutil.rmtree(str(samples_dir / 'package2' /'dist'))
+    except FileNotFoundError:
+        pass
+    wheel_main(samples_dir / 'package2' / 'package2-pkg.ini')
+    assert_isfile(samples_dir / 'package2' / 'dist/package2-0.1-py2.py3-none-any.whl')
+
 def test_dist_name():
     clear_samples_dist()
     wheel_main(samples_dir / 'altdistname.ini')
