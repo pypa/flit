c065d7bcafac156937d7708b20808281bbbaa421
Thomas Kluyver <thomas@kluyver.me.uk>
2019-06-28 20:07:26 +0200

    Move test_buildapi to flit_core

--- flit_core/flit_core/tests/test_buildapi.py
+++ flit_core/flit_core/tests/test_buildapi.py
@@ -1,19 +1,19 @@
 from contextlib import contextmanager
 import os
-from pathlib import Path
+import os.path as osp
 import tarfile
 from tempfile import TemporaryDirectory
 from testpath import assert_isfile, assert_isdir
 import zipfile
 
-from flit import buildapi
+from flit_core import buildapi
 
-samples_dir = Path(__file__).parent / 'samples'
+samples_dir = osp.join(osp.dirname(__file__), 'samples')
 
 @contextmanager
 def cwd(directory):
     prev = os.getcwd()
-    os.chdir(str(directory))
+    os.chdir(directory)
     try:
         yield
     finally:
@@ -21,27 +21,27 @@ def cwd(directory):
 
 def test_get_build_requires():
     expected = ["requests >= 2.18", "docutils"]
-    with cwd(samples_dir / 'pep517'):
+    with cwd(osp.join(samples_dir,'pep517')):
         assert buildapi.get_requires_for_build_wheel() == expected
         assert buildapi.get_requires_for_build_sdist() == expected
 
 def test_build_wheel():
-    with TemporaryDirectory() as td, cwd(samples_dir / 'pep517'):
+    with TemporaryDirectory() as td, cwd(osp.join(samples_dir,'pep517')):
         filename = buildapi.build_wheel(td)
         assert filename.endswith('.whl'), filename
-        assert_isfile(Path(td, filename))
-        assert zipfile.is_zipfile(str(Path(td, filename)))
+        assert_isfile(osp.join(td, filename))
+        assert zipfile.is_zipfile(osp.join(td, filename))
 
 def test_build_sdist():
-    with TemporaryDirectory() as td, cwd(samples_dir / 'pep517'):
+    with TemporaryDirectory() as td, cwd(osp.join(samples_dir,'pep517')):
         filename = buildapi.build_sdist(td)
         assert filename.endswith('.tar.gz'), filename
-        assert_isfile(Path(td, filename))
-        assert tarfile.is_tarfile(str(Path(td, filename)))
+        assert_isfile(osp.join(td, filename))
+        assert tarfile.is_tarfile(osp.join(td, filename))
 
 def test_prepare_metadata_for_build_wheel():
-    with TemporaryDirectory() as td, cwd(samples_dir / 'pep517'):
+    with TemporaryDirectory() as td, cwd(osp.join(samples_dir,'pep517')):
         dirname = buildapi.prepare_metadata_for_build_wheel(td)
         assert dirname.endswith('.dist-info'), dirname
-        assert_isdir(Path(td, dirname))
-        assert_isfile(Path(td, dirname, 'METADATA'))
+        assert_isdir(osp.join(td, dirname))
+        assert_isfile(osp.join(td, dirname, 'METADATA'))
