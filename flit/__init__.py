26a71f053d2b8baf6f185aa2aff58673a2521ac2
Thomas Kluyver <thomas@kluyver.me.uk>
2019-11-23 13:36:18 +0000

    Version number -> 2.0.2

--- flit/__init__.py
+++ flit/__init__.py
@@ -10,7 +10,7 @@ from flit_core import common
 from .inifile import ConfigError
 from .log import enable_colourful_output
 
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 log = logging.getLogger(__name__)
 
