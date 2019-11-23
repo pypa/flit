070370d3fe7322bd964376bb50bfd3550776b9fb
Thomas Kluyver <thomas@kluyver.me.uk>
2019-11-23 13:18:22 +0000

    Version number -> 2.0.1

--- doc/conf.py
+++ doc/conf.py
@@ -56,7 +56,7 @@ copyright = u'2015, Thomas Kluyver'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version #+ '.1'
 
