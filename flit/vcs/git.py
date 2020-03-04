443d8edcabd13a1ef0e439fde31126d6c87bb9e9
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-03-04 22:30:33 +0700

    Handle git submodules inclusion in sdist (fix #323)

--- flit/vcs/git.py
+++ flit/vcs/git.py
@@ -4,7 +4,8 @@ from subprocess import check_output
 name = 'git'
 
 def list_tracked_files(directory):
-    outb = check_output(['git', 'ls-files'], cwd=str(directory))
+    outb = check_output(['git', 'ls-files', '--recurse-submodules'],
+                        cwd=str(directory))
     return [os.fsdecode(l) for l in outb.strip().splitlines()]
 
 def list_untracked_deleted_files(directory):
