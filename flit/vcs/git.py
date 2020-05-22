6d5f2fc84eb3a1485ee7bd583b2ae7e03dae2090
Brendan Abel <007brendan@gmail.com>
2020-05-21 20:42:56 -0700

    fix: correctly handle unicode in filenames when building sdists
    fix to the git vcs module to prevent git from double escaping
    unicode chars in filenames so that flit can decode them correctly
    
    fixes #345

--- flit/vcs/git.py
+++ flit/vcs/git.py
@@ -4,12 +4,12 @@ from subprocess import check_output
 name = 'git'
 
 def list_tracked_files(directory):
-    outb = check_output(['git', 'ls-files', '--recurse-submodules'],
+    outb = check_output(['git', 'ls-files', '--recurse-submodules', '-z'],
                         cwd=str(directory))
-    return [os.fsdecode(l) for l in outb.strip().splitlines()]
+    return [os.fsdecode(l) for l in outb.strip(b'\0').split(b'\0') if l]
 
 def list_untracked_deleted_files(directory):
     outb = check_output(['git', 'ls-files', '--deleted', '--others',
-                         '--exclude-standard'],
+                         '--exclude-standard', '-z'],
                         cwd=str(directory))
-    return [os.fsdecode(l) for l in outb.strip().splitlines()]
+    return [os.fsdecode(l) for l in outb.strip(b'\0').split(b'\0') if l]
