b07e35e03a7fa13863bde38e317c809d7cbb34e1
vexelnet <vexel@vexel.net>
2020-08-16 15:49:31 +0200

    Reproducibly produce RECORD file

--- flit/install.py
+++ flit/install.py
@@ -392,7 +392,7 @@ class Installer(object):
 
         with (dist_info / 'RECORD').open('w', encoding='utf-8') as f:
             cf = csv.writer(f)
-            for path in self.installed_files:
+            for path in sorted(self.installed_files, key=lambda s: str(s)):
                 path = pathlib.Path(path)
                 if path.is_symlink() or path.suffix in {'.pyc', '.pyo'}:
                     hash, size = '', ''
