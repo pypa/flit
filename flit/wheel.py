2a8785811f61f9959e91376fa6337e677c94ce0e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-03-20 15:11:21 +0100

    Add PEP 660 support

--- flit/wheel.py
+++ flit/wheel.py
@@ -4,8 +4,8 @@ import flit_core.wheel as core_wheel
 
 log = logging.getLogger(__name__)
 
-def make_wheel_in(ini_path, wheel_directory):
-    return core_wheel.make_wheel_in(ini_path, wheel_directory)
+def make_wheel_in(ini_path, wheel_directory, editable=False):
+    return core_wheel.make_wheel_in(ini_path, wheel_directory, editable)
 
 class WheelBuilder(core_wheel.WheelBuilder):
     pass
