3434942511a3949896d55543a88875a7e7661cfa
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-06 12:27:01 -0700

    added .git to both methodss

--- flit/vcs/git.py
+++ flit/vcs/git.py
@@ -6,7 +6,7 @@ name = "git"
 
 def list_tracked_files(directory):
     outb = check_output(
-        ["git", "ls-files", "--recurse-submodules", "-z"], cwd=str(directory)
+        ["git", ".git", "ls-files", "--recurse-submodules", "-z"], cwd=str(directory)
     )
     return [os.fsdecode(l) for l in outb.strip(b"\0").split(b"\0") if l]
 
