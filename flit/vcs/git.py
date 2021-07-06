fd806306acf7e39d0c9204cc3eaa8b2a5ae9c78f
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-06 12:19:18 -0700

    added check for .git

--- flit/vcs/git.py
+++ flit/vcs/git.py
@@ -1,15 +1,27 @@
 import os
 from subprocess import check_output
 
-name = 'git'
+name = "git"
+
 
 def list_tracked_files(directory):
-    outb = check_output(['git', 'ls-files', '--recurse-submodules', '-z'],
-                        cwd=str(directory))
-    return [os.fsdecode(l) for l in outb.strip(b'\0').split(b'\0') if l]
+    outb = check_output(
+        ["git", "ls-files", "--recurse-submodules", "-z"], cwd=str(directory)
+    )
+    return [os.fsdecode(l) for l in outb.strip(b"\0").split(b"\0") if l]
+
 
 def list_untracked_deleted_files(directory):
-    outb = check_output(['git', 'ls-files', '--deleted', '--others',
-                         '--exclude-standard', '-z'],
-                        cwd=str(directory))
-    return [os.fsdecode(l) for l in outb.strip(b'\0').split(b'\0') if l]
+    outb = check_output(
+        [
+            "git",
+            ".git",
+            "ls-files",
+            "--deleted",
+            "--others",
+            "--exclude-standard",
+            "-z",
+        ],
+        cwd=str(directory),
+    )
+    return [os.fsdecode(l) for l in outb.strip(b"\0").split(b"\0") if l]
