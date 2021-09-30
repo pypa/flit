63d45f74b70bb1d5305e2ce289815bd1042f6260
Matthew Willcockson <matthew@willcockson.family>
2021-09-30 04:10:22 -0500

    test: split out untracked files

--- tests/test_build.py
+++ tests/test_build.py
@@ -13,12 +13,22 @@ samples_dir = Path(__file__).parent / 'samples'
 LIST_FILES_TEMPLATE = """\
 #!{python}
 import sys
+tracked = {tracked}
 if '--deleted' not in sys.argv:
+    for filename in tracked:
+        print(filename, end="\\0")
+else:
     from pathlib import Path, PurePosixPath
     cwd = Path.cwd()
-    git_dir = (cwd / ".git")
+    git_dir = cwd / ".git"
+    tracked = [(cwd / filename) for filename in tracked]
     for path in cwd.rglob("*"):
-        if git_dir in path.parents or path in [cwd, git_dir] or not path.is_file():
+        if (
+            not path.is_file()
+            or path in tracked
+            or git_dir in path.parents
+            or path in [cwd, git_dir]
+        ):
             continue
         relative_path = path.relative_to(cwd)
         linux_path = PurePosixPath().joinpath(*relative_path.parts)
@@ -28,9 +38,14 @@ if '--deleted' not in sys.argv:
 def test_build_main(copy_sample):
     td = copy_sample('module1_toml')
     (td / '.git').mkdir()   # Fake a git repo
+    tracked = [
+        str(path.relative_to(samples_dir / "module1_toml"))
+        for path in (samples_dir / "module1_toml").rglob("*")
+        if path.is_file()
+    ]
 
     with MockCommand('git', LIST_FILES_TEMPLATE.format(
-            python=sys.executable)):
+            python=sys.executable, tracked=tracked)):
         res = build.main(td / 'pyproject.toml')
     assert res.wheel.file.suffix == '.whl'
     assert res.sdist.file.name.endswith('.tar.gz')
@@ -40,9 +55,14 @@ def test_build_main(copy_sample):
 def test_build_sdist_only(copy_sample):
     td = copy_sample('module1_toml')
     (td / '.git').mkdir()  # Fake a git repo
+    tracked = [
+        str(path.relative_to(samples_dir / "module1_toml"))
+        for path in (samples_dir / "module1_toml").rglob("*")
+        if path.is_file()
+    ]
 
     with MockCommand('git', LIST_FILES_TEMPLATE.format(
-            python=sys.executable)):
+            python=sys.executable, tracked=tracked)):
         res = build.main(td / 'pyproject.toml', formats={'sdist'})
     assert res.wheel is None
 
@@ -52,9 +72,14 @@ def test_build_sdist_only(copy_sample):
 def test_build_wheel_only(copy_sample):
     td = copy_sample('module1_toml')
     (td / '.git').mkdir()  # Fake a git repo
+    tracked = [
+        str(path.relative_to(samples_dir / "module1_toml"))
+        for path in (samples_dir / "module1_toml").rglob("*")
+        if path.is_file()
+    ]
 
     with MockCommand('git', LIST_FILES_TEMPLATE.format(
-            python=sys.executable)):
+            python=sys.executable, tracked=tracked)):
         res = build.main(td / 'pyproject.toml', formats={'wheel'})
     assert res.sdist is None
 
@@ -81,10 +106,16 @@ def test_build_module_no_docstring():
         shutil.copy(str(samples_dir / 'no_docstring.py'), td)
         shutil.copy(str(samples_dir / 'EG_README.rst'), td)
         Path(td, '.git').mkdir()   # Fake a git repo
+        tracked = [
+            "pyproject.toml",
+            "no_docstring-pkg.toml",
+            "no_docstring.py",
+            "EG_README.rst",
+        ]
 
 
         with MockCommand('git', LIST_FILES_TEMPLATE.format(
-                python=sys.executable)):
+                python=sys.executable, tracked=tracked)):
             with pytest.raises(common.NoDocstringError) as exc_info:
                 build.main(pyproject)
             assert 'no_docstring.py' in str(exc_info.value)
