47c1b48b307f4c89fe9178b1fca2282950e213cb
Thomas Kluyver <thomas@kluyver.me.uk>
2021-10-01 09:46:39 +0100

    Add test for usable SOURCE_DATE_EPOCH value

--- flit_core/flit_core/tests/test_wheel.py
+++ flit_core/flit_core/tests/test_wheel.py
@@ -12,6 +12,16 @@ def test_licenses_dir(tmp_path):
     info = make_wheel_in(samples_dir / 'inclusion' / 'pyproject.toml', tmp_path)
     assert_isfile(info.file)
 
+
+def test_source_date_epoch(tmp_path, monkeypatch):
+    monkeypatch.setenv('SOURCE_DATE_EPOCH', '1633007882')
+    info = make_wheel_in(samples_dir / 'pep621' / 'pyproject.toml', tmp_path)
+    assert_isfile(info.file)
+    # Minimum value for zip timestamps is 1980-1-1
+    with ZipFile(info.file, 'r') as zf:
+        assert zf.getinfo('module1a.py').date_time[:3] == (2021, 9, 30)
+
+
 def test_zero_timestamp(tmp_path, monkeypatch):
     monkeypatch.setenv('SOURCE_DATE_EPOCH', '0')
     info = make_wheel_in(samples_dir / 'pep621' / 'pyproject.toml', tmp_path)
