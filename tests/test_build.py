7a3fdcf4c589c8302e9a9b9782f682200ef17946
Thomas Kluyver <thomas@kluyver.me.uk>
2021-11-17 20:33:04 +0000

    Use copy_sample in build test for namespace package

--- tests/test_build.py
+++ tests/test_build.py
@@ -55,25 +55,17 @@ def test_build_wheel_only(copy_sample):
     # Compare str path to work around pathlib/pathlib2 mismatch on Py 3.5
     assert [str(p) for p in (td / 'dist').iterdir()] == [str(res.wheel.file)]
 
-def test_build_ns_main():
-    with TemporaryDirectory() as td:
-        pyproject = Path(td, 'pyproject.toml')
-        shutil.copy(str(samples_dir / 'ns1-pkg' / 'pyproject.toml'), str(pyproject))
-        ns = Path(td, 'ns1')
-        ns.mkdir()
-        pkg = ns / 'pkg'
-        pkg.mkdir()
-        shutil.copy(str(samples_dir / 'ns1-pkg' / 'ns1' / 'pkg' / '__init__.py'), str(pkg))
-        shutil.copy(str(samples_dir / 'ns1-pkg' / 'EG_README.rst'), td)
-        Path(td, '.git').mkdir()   # Fake a git repo
+def test_build_ns_main(copy_sample):
+    td = copy_sample('ns1-pkg')
+    (td / '.git').mkdir()   # Fake a git repo
 
-        with MockCommand('git', LIST_FILES_TEMPLATE.format(
-                python=sys.executable, module='ns1/pkg/__init__.py')):
-            res = build.main(pyproject)
-        assert res.wheel.file.suffix == '.whl'
-        assert res.sdist.file.name.endswith('.tar.gz')
+    with MockCommand('git', LIST_FILES_TEMPLATE.format(
+            python=sys.executable, module='ns1/pkg/__init__.py')):
+        res = build.main(td / 'pyproject.toml')
+    assert res.wheel.file.suffix == '.whl'
+    assert res.sdist.file.name.endswith('.tar.gz')
 
-        assert_isdir(Path(td, 'dist'))
+    assert_isdir(td / 'dist')
 
 
 def test_build_module_no_docstring():
