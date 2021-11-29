1009458e9cc1433e043576c805371b506dda4c18
James Hilliard <james.hilliard1@gmail.com>
2021-11-29 01:40:27 -0700

    Generate a setup.py for flit_core for bootstrapping.

--- flit_core/build_dists.py
+++ flit_core/build_dists.py
@@ -3,13 +3,36 @@
 Normally, this should only be used by me when making a release.
 """
 import os
+import sys
+from pathlib import Path
 
+cwd = Path(__file__).parent.resolve()
+os.chdir(cwd)
+sys.path.append(str(cwd.parent))
+# Allow importing from flit without requests(not needed)
+sys.modules['requests'] = False
+from flit.sdist import SdistBuilder
 from flit_core import build_thyself
+from flit_core.build_thyself import metadata
+from flit_core.common import Module
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def build_release_sdist(sdist_directory, config_settings=None):
+    """Builds an sdist, places it in sdist_directory"""
+    cwd = Path.cwd()
+    module = Module('flit_core', cwd)
+    reqs_by_extra = {'.none': metadata.requires}
+
+    sb = SdistBuilder(
+        module, metadata, cwd, reqs_by_extra, entrypoints={},
+        extra_files=['pyproject.toml', 'build_dists.py']
+    )
+    path = sb.build(Path(sdist_directory), gen_setup_py=True)
+    return path.name
+
 
 print("Building sdist")
-sdist_fname = build_thyself.build_sdist('dist/')
+sdist_fname = build_release_sdist('dist/')
 print(os.path.join('dist', sdist_fname))
 
 print("\nBuilding wheel")
