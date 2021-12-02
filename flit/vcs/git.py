aadee0d334681a3f77b6e284c897b948ae15a0a9
Matthew Willcockson <matthew@willcockson.family>
2021-12-02 08:27:49 -0600

    add comment about os.fsdecode()

--- flit/vcs/git.py
+++ flit/vcs/git.py
@@ -7,6 +7,8 @@ name = 'git'
 def list_tracked_files(directory):
     outb = check_output(['git', 'ls-files', '--recurse-submodules', '-z'],
                         cwd=str(directory))
+    # NOTE: os.fsdecode() may cause issues if git returns path names that
+    # aren't in the filesystem encoding
     return [osp.normpath(os.fsdecode(l)) for l in outb.strip(b'\0').split(b'\0') if l]
 
 def list_untracked_deleted_files(directory):
