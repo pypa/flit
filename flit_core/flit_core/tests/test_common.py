77f92ca8eae79e891a12c1b5c58c853b6521578e
Pavel 'LEdoian' Turinsky <ledoian@matfyz.cz>
2022-08-26 23:02:25 +0200

    Add tests for `~=` requirement syntax

--- flit_core/flit_core/tests/test_common.py
+++ flit_core/flit_core/tests/test_common.py
@@ -113,6 +113,8 @@ def test_normalize_file_permissions():
         ("<4, > 3.2", False),
         (">3.4", False),
         (">=2.7, !=3.0.*, !=3.1.*, !=3.2.*", True),
+        ("~3.3", False),
+        ("~2.7", True),
     ],
 )
 def test_supports_py2(requires_python, expected_result):
