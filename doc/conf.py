389c23c797f203762095daa0d100a5d6fa198d45
Thomas Kluyver <thomas@kluyver.me.uk>
2025-02-18 12:08:58 +0000

    Bump version: 3.10.1 → 3.11.0

--- doc/conf.py
+++ doc/conf.py
@@ -57,7 +57,7 @@ copyright = u'2015, Thomas Kluyver'
 # built documents.
 #
 # The short X.Y version.
-version = '3.10.1'
+version = '3.11.0'
 # The full version, including alpha/beta/rc tags.
 release = version #+ '.1'
 
