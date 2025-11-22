bd73eeedd3a0965d971b7bc5f8ed20e5f984a5fa
Thomas Kluyver <thomas@kluyver.me.uk>
2025-11-22 11:15:12 +0000

    Remove flit.buildapi module

--- flit/buildapi.py
+++
@@ -1,6 +0,0 @@
-from warnings import warn
-warn('A package has specified `build-backend = "flit.buildapi"` and is being '
-     'built with Flit >= 3.10. This is likely to break in a future version. '
-     'Please change the backend to flit_core.buildapi, and/or specify a '
-     'maximum version of Flit.')
-from flit_core.buildapi import *
